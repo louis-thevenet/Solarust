5cbbe61f95850c3cd983c55a59247cce9d0ec501
Louis Thevenet <louis.tvnt@gmail.com>
2024-06-01 10:39:57 +0200

    feat: basic UI with Play/Pause/Quit buttons

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use bevy::prelude::*;
 use camera::camera_plugin::CustomCameraPlugin;
 use planets::planet_plugin::PlanetPlugin;
+use ui::UIPlugin;
 mod camera;
 mod planets;
+mod ui;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins((CustomCameraPlugin, PlanetPlugin))
+        .add_plugins((UIPlugin, CustomCameraPlugin, PlanetPlugin))
         .run();
 }
---
+++ src/ui.rs
@@ -0,0 +1,66 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
+#[derive(Default, States, Debug, Hash, Eq, Clone, Copy, PartialEq)]
+
+pub enum SimulationState {
+    #[default]
+    Paused,
+    Running,
+}
+
+#[derive(Default, Resource)]
+pub struct AppConfig {}
+
+pub struct UIPlugin;
+impl Plugin for UIPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<AppConfig>()
+            .init_state::<SimulationState>()
+            .add_plugins(EguiPlugin)
+            //.add_systems(Startup, configure_app_state)
+            .add_systems(Update, (build_ui, ui_controls));
+    }
+}
+
+fn build_ui(
+    mut contexts: EguiContexts,
+    sim_state: Res<State<SimulationState>>,
+    mut next_sim_state: ResMut<NextState<SimulationState>>,
+) {
+    egui::SidePanel::left("Menu")
+        .resizable(true)
+        .show(contexts.ctx_mut(), |ui| {
+            match *sim_state.get() {
+                SimulationState::Paused => {
+                    ui.heading("Paused");
+                    if ui.button("Resume").clicked() {
+                        next_sim_state.set(SimulationState::Running)
+                    }
+                }
+                SimulationState::Running => {
+                    ui.heading("Running");
+                    if ui.button("Pause").clicked() {
+                        next_sim_state.set(SimulationState::Paused)
+                    }
+                }
+            }
+
+            if ui.button("Quit").clicked() {
+                std::process::exit(0);
+            };
+        });
+}
+
+fn ui_controls(
+    keys: Res<ButtonInput<KeyCode>>,
+    sim_state: Res<State<SimulationState>>,
+    mut next_sim_state: ResMut<NextState<SimulationState>>,
+) {
+    if keys.just_pressed(KeyCode::Space) {
+        match sim_state.get() {
+            SimulationState::Paused => next_sim_state.set(SimulationState::Running),
+            SimulationState::Running => next_sim_state.set(SimulationState::Paused),
+        }
+    }
+}
