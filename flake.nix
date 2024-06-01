8500f50e53576a099b16f691c8172979f0d4334c
Louis Thevenet <louis.tvnt@gmail.com>
2024-06-01 17:39:59 +0200

    feat: gh action to build to wasm

--- flake.nix
+++ flake.nix
@@ -1,72 +1,64 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    fenix.url = "github:nix-community/fenix";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-  };
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
-      imports = [
-        inputs.treefmt-nix.flakeModule
-      ];
 
-      perSystem = {
-        config,
-        self',
-        pkgs,
-        lib,
-        system,
-        ...
-      }: let
-        fenixPkgs = inputs.fenix.packages.${system};
-        toolchain = fenixPkgs.combine [
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self,nixpkgs,fenix}:
+
+        let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { system = system; };
+        fenixPkgs = fenix.packages.${system};
+   toolchain = fenixPkgs.combine [
           # Default Rust tools
           fenixPkgs.stable.cargo
           fenixPkgs.stable.clippy
           fenixPkgs.stable.rust-src
           fenixPkgs.stable.rustc
           fenixPkgs.stable.rustfmt
+          # Needed by engine WASM build
+          fenixPkgs.targets.wasm32-unknown-unknown.stable.rust-std
         ];
 
-        nativeBuldInputs = [pkgs.pkg-config];
+nativeBuldInputs = [pkgs.pkg-config];
 
-        buildInputs = with pkgs; [
-          udev
-          alsa-lib
-          vulkan-loader
-          libxkbcommon
-          wayland
+          buildInputs = with pkgs; [
+           udev alsa-lib vulkan-loader
+            libxkbcommon wayland
+          ];
+
+          wasmBuildInputs = [
+          pkgs.wasm-pack
+          pkgs.wasm-bindgen-cli
         ];
-      in {
-        # Rust dev environment
-        devShells.default = pkgs.mkShell {
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
 
-          RUST_BACKTRACE = "full";
-          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+        in
+{
+          # Rust dev environment
+          devShells."x86_64-linux".default = pkgs.mkShell {
 
-          packages =
-            buildInputs
-            ++ [toolchain]
-            ++ nativeBuldInputs
-            ++ [
-              # Nix tools
-              pkgs.nil
-              pkgs.alejandra
-            ];
-        };
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
 
-        # Add your auto-formatters here.
-        # cf. https://numtide.github.io/treefmt/
-        treefmt.config = {
-          projectRootFile = "flake.nix";
-          programs = {
-            nixpkgs-fmt.enable = true;
-            rustfmt.enable = true;
+            RUST_BACKTRACE = "full";
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+
+            packages = buildInputs ++ [toolchain]  ++ wasmBuildInputs ++ nativeBuldInputs;
+            shellHook=''export CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUNNER=wasm-server-runner'';
+          };
+
+          # Add your auto-formatters here.
+          # cf. https://numtide.github.io/treefmt/
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixpkgs-fmt.enable = true;
+              rustfmt.enable = true;
+            };
           };
         };
-      };
-    };
-}
+    }
